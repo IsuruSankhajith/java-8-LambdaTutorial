d2fd8c00b3f877c0bfeb162feb152cb741054867
Isuru sankhajith <isankhajith@gmail.com>
2024-07-27 23:27:04 +0530

    Modified Code

---
+++ src/javalambdabasics/UnitExcersice1.java
@@ -0,0 +1,19 @@
+package javalambdabasics;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UnitExcersice1 {
+
+	public static void main(String[] args) {
+		List<Person> people = Arrays.asList(
+				new Person("Isuru","Sankhajith",10),
+				new Person("Iu","Shajith",30),
+				new Person("uru","nkhajith",50),
+				new Person("suru","nkhajith",60),
+				new Person("u","ankhajith",70)
+				);
+
+	}
+
+}
